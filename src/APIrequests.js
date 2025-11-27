9516d59fbff9b985a4474296d6107e65fc09d5cc
Saejin <saejinmx@gmail.com>
2025-11-27 18:19:30 +0100

    Récupérations des données fonctionnelles

---
+++ src/APIrequests.js
@@ -0,0 +1,50 @@
+export default class APIrequest {
+    constructor() {
+        this.myToken = import.meta.env.VITE_GITHUB_TOKEN;
+        this.parameters = '';
+
+        this.init();
+    };
+
+    init() {
+        this.setParameters();
+    };
+
+    setParameters() {
+        this.parameters = {
+            headers: {
+                Authorization: `token ${this.myToken}`,
+            },
+        };
+    };
+
+    async getResult(url) {
+        try {
+            const response = await fetch(url, {...this.parameters});
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error: ', error);
+        }
+    }
+
+    async getSearchResults(username) {
+        const url = `https://api.github.com/search/users?q=${username}`;
+        return await this.getResult(url);
+    }
+
+    async getUserDetails(username) {
+        const url = `https://api.github.com/users/${username}`;
+        return await this.getResult(url);
+    }
+
+    async getUserRepos(username) {
+        const url = `https://api.github.com/users/${username}/repos`;
+        return await this.getResult(url);
+    }
+
+    async getRepoCommits(username, repo) {
+        const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+        return await this.getResult(url);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 import './style.css';
+import APIrequests from './APIrequests.js';
+
+new APIrequests();
 
 console.log('GitHub Explorer');
+
+
+// const results = await new APIrequests().getSearchResults('ISaejinI');
+// console.log(results);
+
+// const userDetails = await new APIrequests().getUserDetails('ISaejinI');
+// console.log(userDetails);
+
+// const userRepos = await new APIrequests().getUserRepos('ISaejinI');
+// console.log(userRepos);
+
+// const repoCommits = await new APIrequests().getRepoCommits('ISaejinI', 'GitHub-Inspector');
+// console.log(repoCommits);
