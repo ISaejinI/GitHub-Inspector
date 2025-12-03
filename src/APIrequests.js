487ce7c1baf8e7997bc96d433d5a452802f26c22
Saejin <saejinmx@gmail.com>
2025-12-04 00:10:51 +0100

    css non fonctionnel

--- src/APIrequests.js
+++ src/APIrequests.js
@@ -26,25 +26,31 @@ export default class APIrequest {
         } catch (error) {
             console.error('Error: ', error);
         }
-    }
+    };
 
     async getSearchResults(username) {
         const url = `https://api.github.com/search/users?q=${username}`;
+        // retourne aussi le status (pending, fulfilled, rejected)
         return await this.getResult(url);
-    }
+    };
 
     async getUserDetails(username) {
         const url = `https://api.github.com/users/${username}`;
         return await this.getResult(url);
-    }
+    };
 
     async getUserRepos(username) {
         const url = `https://api.github.com/users/${username}/repos`;
         return await this.getResult(url);
-    }
+    };
 
     async getRepoCommits(username, repo) {
         const url = `https://api.github.com/repos/${username}/${repo}/commits`;
         return await this.getResult(url);
+    };
+
+    displayLoader(id) {
+        // Afficher le loader
+        document.getElementById(id).classList.remove('hide');
     }
 }
--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ new APIrequests();
 console.log('GitHub Explorer');
 
 
-const results = await new APIrequests().getSearchResults('ISaejinI');
-console.log(results);
+// const results = await new APIrequests().getSearchResults('ISaejinI');
+// console.log(results);
 
 // const userDetails = await new APIrequests().getUserDetails('ISaejinI');
 // console.log(userDetails);
@@ -17,3 +17,32 @@ console.log(results);
 
 // const repoCommits = await new APIrequests().getRepoCommits('ISaejinI', 'GitHub-Inspector');
 // console.log(repoCommits);
+
+
+// Au clic du bouton de recherche, récupérer la valeur de l'input et lancer la recherche
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const searchForm = document.getElementById('search_form');
+
+    searchForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const searchInput = document.getElementById('searchBar');
+        const username = searchInput.value.trim();
+
+        if (username) {
+            const results = await new APIrequests().getSearchResults(username);
+            console.log(results.items);
+
+
+
+
+            const result = new Promise ((resolve, reject) => {
+
+            })
+
+        }
+    });
+});
+
